96f72373563f38a334797c87d6b344a8b56c9fa2
eshaan-vimal <evk2.development@gmail.com>
2025-04-22 04:52:55 +0530

    updated app

--- frontend/lib/core/constants/constants.dart
+++ frontend/lib/core/constants/constants.dart
@@ -1,5 +1,5 @@
 class Constants
 {
   // https://tasks-app-backend-rkxf.onrender.com
-  static String backendUri = "http://10.0.2.2:8000";
+  static String backendUri = "https://tasks-app-backend-rkxf.onrender.com";
 }
